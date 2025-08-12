538ccdcd220958ebd6a5c18ae29eefeb591ecbfb
VasathanPriN <priyan.udayakumar@gmail.com>
2025-08-12 22:45:27 +0530

    Created

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 import App from "./App.jsx";
 import "./index.css";
-
+serviceWorkerRegistration.register();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
